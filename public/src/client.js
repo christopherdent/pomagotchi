4746f31aae140dba5d1aea49e9a02f840d747022
Christopher Dent <christopherdent01@gmail.com>
2021-03-06 18:23:55 -0500

    Outline basic project structure and properties object

--- public/src/client.js
+++ public/src/client.js
@@ -11,37 +11,43 @@
 //   }
 // }
 
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
-
+  // function petInfo(){
+    const petInfo = {
+      name: "Chilito",
+      weight: "22",
+      happiness: "0"
+    }
+  // }
 
 
 function makeCards() {
-let img = document.createElement("img");
-  img.src = './assets/pom.jpg'
-const main = document.getElementById("main")
-const pupCard = document.createElement("DIV");
-  pupCard.setAttribute("class", "card")
-  pupCard.setAttribute("id", "pupCard")
-const statsCard = document.createElement("DIV")
-  statsCard.setAttribute("class", "card")
-  statsCard.setAttribute("id", "statsCard")
-main.appendChild(statsCard)
-main.appendChild(pupCard)
-pupCard.appendChild(img)
-  createButtons()
-  displayStats()
-
+  let img = document.createElement("IMG");
+    img.src = './assets/pom.jpg'
+  const main = document.getElementById("main")
+  const pupCard = document.createElement("DIV");
+    pupCard.setAttribute("class", "card")
+    pupCard.setAttribute("id", "pupCard")
+  const statsCard = document.createElement("DIV")
+    statsCard.setAttribute("class", "card")
+    statsCard.setAttribute("id", "statsCard")
+  main.appendChild(statsCard)
+  main.appendChild(pupCard)
+  pupCard.appendChild(img)
+    createButtons()
+    displayStats()
 }
 
 function displayStats(){
 const statsCard = document.getElementById("statsCard")
   let name = document.createElement("P") /// innerHTML of P should be "weight + {function that determines weight}"
-    name.innerHTML = "My name is:"
+    name.innerHTML = `My name is: ${petInfo.name}`
   let weight = document.createElement("P")
-    weight.innerHTML = "I weigh..."
+    weight.innerHTML = `I weigh ${petInfo.weight}`
   let happiness = document.createElement("P")
-    happiness.innerHTML= "My happiness level is:"
+    happiness.innerHTML= `My happiness level is ${petInfo.happiness}`
     statsCard.appendChild(name);
     statsCard.appendChild(weight);
     statsCard.appendChild(happiness);
@@ -50,23 +56,34 @@ const statsCard = document.getElementById("statsCard")
 
 function createButtons() {
   const pupCard = document.getElementById("pupCard")
-  let btn = document.createElement("BUTTON");
-  btn.innerHTML = "Feed me!"
-  btn.setAttribute("class", "btn btn-primary")
-  pupCard.appendChild(btn);
+  const br = document.createElement("BR");
+
+  // for ( var i =0; i < 20; i++ ) { pupCard.appendChild(br) }
+  pupCard.appendChild(br)
+
+
+  let feedButton = document.createElement("BUTTON");
+    feedButton.innerHTML = "Feed me!"
+    feedButton.setAttribute("class", "btn btn-primary")
+    pupCard.appendChild(feedButton);
+    pupCard.innerHTML += '  ';
+  let playButton = document.createElement("BUTTON");
+    playButton.innerHTML = "Play With Me!"
+    playButton.setAttribute("class", "btn btn-success")
+    pupCard.appendChild(playButton);
+    pupCard.innerHTML += '  ';
+  let exerciseButton = document.createElement("BUTTON");
+    exerciseButton.innerHTML = "Make me exercise!"
+    exerciseButton.setAttribute("class", "btn btn-danger")
+    pupCard.appendChild(exerciseButton);
+
+
 }
 
 
 
-function petInfo(){
-  const petInfo = {
-    name: "Chilito",
-    weight: "22",
-    happiness: "0"
-  }
-}
 
-console.log("do i work")
+
 
 function checkAndUpdatePetInfoInHtml() {
 //click buttons

90cd1b7054e2b8899506cae6f2f68feec65ce8af
christopherdent <christopherdent01@gmail.com>
2021-05-17 22:50:17 -0400

    fix ager function to work with int instead of str

--- public/src/game.js
+++ public/src/game.js
@@ -36,7 +36,7 @@ class Game {
          //  happiness.innerHTML= `My happiness level is ${petInfo.happiness}`
           happiness.setAttribute('id', 'happinessField')
        let age = document.createElement("P")
-         age.innerHTML = `I am ${newPet.age} months old.`
+        //  age.innerHTML = `I am ${newPet.age} months old.`
          age.setAttribute('id', 'ageField')
           statsCard.appendChild(name);
           statsCard.appendChild(weight);
@@ -84,25 +84,25 @@ class Game {
    
      
    
-      //  onSaveClick = () => {
+       onSaveClick = () => {
 
-      //    console.log("saving")
-      //    let configObj = {
-      //     method: "PATCH",
-      //     headers:  {
-      //       "Content-Type": "application/json",
-      //       "Accept": "application/json"
-      //     },
-      //     // body: JSON.stringify(newPet)
-      //     body: JSON.stringify( { pet: newPet } )   
-      //   };
+         console.log("saving")
+         let configObj = {
+          method: "PATCH",
+          headers:  {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          // body: JSON.stringify(newPet)
+          body: JSON.stringify( { pet: newPet } )   
+        };
 
-      //   fetch(`http://localhost:3000/api/v1/pets/${newPet.id}`, configObj)
-      //   .then(r => r.json())
-      //   .then(json => {
-      //     console.log(json)
-      //   })
-      //  }
+        fetch(`http://localhost:3000/api/v1/pets/${newPet.id}`, configObj)
+        .then(r => r.json())
+        .then(json => {
+          console.log(json)
+        })
+       }
   } 
 
 
